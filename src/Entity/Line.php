513b94823981e121ee4feb72643d12f7389093d4
JinChristine <jin.christine_0111@hotmail.com>
2025-02-14 15:24:17 +0000

    update entity Line

--- src/Entity/Line.php
+++ src/Entity/Line.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\LineRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: LineRepository::class)]
@@ -12,20 +11,19 @@ class Line
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private ?int $id = null;
+    private ?int $idLine = null;
 
     #[ORM\Column(length: 255)]
     private ?string $nameLine = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $IdLine = null;
-
-    #[ORM\Column(type: Types::JSON)]
-    private array $stations = [];
-
     public function getId(): ?int
     {
-        return $this->id;
+        return $this->idLine;
+    }
+
+    public function setId(int  $idLine): void
+    {
+        $this->idLine = $idLine;
     }
 
     public function getNameLine(): ?string
@@ -40,27 +38,4 @@ class Line
         return $this;
     }
 
-    public function getIdLine(): ?string
-    {
-        return $this->IdLine;
-    }
-
-    public function setIdLine(string $IdLine): static
-    {
-        $this->IdLine = $IdLine;
-
-        return $this;
-    }
-
-    public function getStations(): array
-    {
-        return $this->stations;
-    }
-
-    public function setStations(array $stations): static
-    {
-        $this->stations = $stations;
-
-        return $this;
-    }
 }
