3e32ac8b884eaf64684dea84bcce378632a0785d
JinChristine <jin.christine_0111@hotmail.com>
2025-02-15 22:30:23 +0100

    create controller: answer, user, game, homepage, rank

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,13 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class LineController extends AbstractController
-{
-    #[Route('/line', name: 'app_line')]
+final class UserController extends AbstractController{
+    #[Route('/user', name: 'ferrovipath_user', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('line/index.html.twig', [
-            'controller_name' => 'LineController',
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
         ]);
     }
 }
