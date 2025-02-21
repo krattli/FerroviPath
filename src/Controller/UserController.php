68ab6e217de3b7c9b2109a261b2bada452232052
JinChristine <jin.christine_0111@hotmail.com>
2025-02-21 14:05:49 +0100

    create profil + start modify

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,6 +14,67 @@ final class UserController extends AbstractController{
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/user/profil/{id}', name: 'ferrovipath_user_profil', methods: ['GET'])]
+    public function profil(int $id): Response
+    {
+        $profils = $this->getProfils();
+        $profil = array_filter($profils, fn($profil) => $profil['id'] === $id);
+
+        if (empty($profil)) {
+            return $this->json(['message' => 'Profil not found'], 404);
+        }
+        return $this->render('user/profil.html.twig',  ['profil' => array_values($profil)[0]]); //, ['user' => array_values($profil)[0]])
+    }
+
+    public function getProfils(): array
+    {
+        return [
+            [
+            'id' => 1,
+            'pseudo' => 'toto',
+            'birth' => '01/12/2000',
+            'email' => 'toto@gmail.com',
+            'password' => 'TOTO',
+            'createdAt' => '16/02/2025'
+            ],
+            [
+            'id' => 2,
+            'pseudo' => 'tata',
+            'birth' => '02/12/2002',
+            'email' => 'tata@gmail.com',
+            'password' => 'TATA',
+            'createdAt' => '16/02/2025'
+            ],
+            [
+            'id' => 3,
+            'pseudo' => 'tuto',
+            'birth' => '21/12/2000',
+            'email' => 'tuto@gmail.com',
+            'password' => 'TUTO',
+            'createdAt' => '16/02/2025']
+        ];
+    }
+
+    #[Route('/user/modify/{id}', name: 'ferrovipath_user_modify', methods: ['GET'])]
+    public function modify(Request $request, int $id): Response
+    {
+        $profils = $this->getProfils();
+        $profil = array_filter($profils, fn($profil) => $profil['id'] === $id);
+        
+        $form = $this->createForm(UserType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            return $this->redirectToRoute('ferrovipath_user_profil');
+        }
+
+        return $this->render('user/modify.html.twig', [
+            'form' => $form->createView(), 'profil' => array_values($profil)[0]
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,73 +5,47 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
     #[ORM\Column]
-    private ?int $idUser = null;
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     #[ORM\Column(length: 255)]
     private ?string $pseudo = null;
 
-    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
-    private ?\DateTimeInterface $birth = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $email = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE)]
+    private ?\DateTimeImmutable $birth = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getIdUser(): ?int
-    {
-        return $this->idUser;
-    }
-
-    public function setIdUser(int $idUser): static
-    {
-        $this->idUser = $idUser;
-
-        return $this;
-    }
-
-    public function getPseudo(): ?string
-    {
-        return $this->pseudo;
-    }
-
-    public function setPseudo(string $pseudo): static
-    {
-        $this->pseudo = $pseudo;
-
-        return $this;
-    }
-
-    public function getBirth(): ?\DateTimeInterface
-    {
-        return $this->birth;
-    }
-
-    public function setBirth(?\DateTimeInterface $birth): static
-    {
-        $this->birth = $birth;
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -86,6 +60,43 @@ class User
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -98,6 +109,27 @@ class User
         return $this;
     }
 
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): static
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->createdAt;
@@ -106,6 +138,18 @@ class User
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getBirth(): ?\DateTimeImmutable
+    {
+        return $this->birth;
+    }
+
+    public function setBirth(\DateTimeImmutable $birth): static
+    {
+        $this->birth = $birth;
 
         return $this;
     }
