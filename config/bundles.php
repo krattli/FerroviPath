2e22d106ee1c2310036d9e8f4ebc8d0dcee08ead
JinChristine <jin.christine_0111@hotmail.com>
2025-02-21 16:34:10 +0100

    add update profil button interaction

--- config/bundles.php
+++ config/bundles.php
@@ -7,6 +7,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
