b2bae48c994e62f5204daa57e3cd3c01dea730fb
amelia <amelia6.ait@gmail.com>
2025-03-15 14:28:04 +0100

    answerPage + navbar

--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -5,14 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-final class AnswerController extends AbstractController
+class AnswerController extends AbstractController
 {
     #[Route('/answer', name: 'ferrovipath_answer', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('answer/index.html.twig', [
-            'controller_name' => 'AnswerController',
+        // Récupère la ligne choisie ou une valeur par défaut
+        $selectedLine = $request->query->get('line', 'default'); 
+
+        // Associe chaque ligne à une image
+        $images = [
+            'metro1' => '/img/m1.png',
+            'metro2' => '/img/metro2.png',
+            'metro3' => '/img/metro3.png',
+        ];
+
+        // Image par défaut
+        $imagePath = $images[$selectedLine] ?? '/img/Logo.png'; 
+
+        // Retourne la page avec l'image sélectionnée
+        return $this->render('answer/lines.html.twig', [
+            'imagePath' => $imagePath,
         ]);
     }
 }
