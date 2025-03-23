47e6fc16701154e36cf48e30dd7dc80b62b92546
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-03-23 21:04:46 +0100

    premier commit game

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Line;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class GameController extends AbstractController
 {
-    #[Route('/game', name: 'ferrovipath_game', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/game/{id}', name: 'ferrovipath_game', methods: ['GET'])]
+    public function index(Line $line): Response
     {
         return $this->render('game/index.html.twig', [
             'controller_name' => 'GameController',
+            'line' => $line
         ]);
     }
 }
--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\LineRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'ferrovipath_homepage', methods: ['GET'])]
-    public function index(): Response
+    public function index(LineRepository $lineRepository): Response
     {
+        $lines = $lineRepository->findAll();
+
+        //dd($lines);
+
         return $this->render('homepage.html.twig', [
             'controller_name' => 'HomePageController',
+            'lines' => $lines
         ]);
     }
 }
