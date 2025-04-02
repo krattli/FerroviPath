a05a8626d735e942168c6d37e50b6e4ab7507f4f
amelia <amelia6.ait@gmail.com>
2025-04-02 12:18:41 +0200

    add icons

--- src/Controller/RankController.php
+++ src/Controller/RankController.php
@@ -1,11 +1,15 @@
 <?php
 
-namespace App\Controller;
+/*namespace App\Controller;
 
+use App\Entity\User; //ajouté
+use App\Entity\Game;//ajouté
+
+use Doctrine\ORM\EntityManagerInterface; //ajouté
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+//
 final class RankController extends AbstractController
 {
     #[Route('/rank', name: 'ferrovipath_rank', methods: ['GET'])]
@@ -17,3 +21,50 @@ final class RankController extends AbstractController
 
     }
 }
+//
+
+
+final class RankController extends AbstractController
+{
+    #[Route('/rank', name: 'ferrovipath_rank', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $query = $entityManager->createQuery(
+            'SELECT u.idUser, u.pseudo, SUM(g.scorePoints) as totalScore
+            FROM App\Entity\Game g
+            JOIN g.user u
+            GROUP BY u.idUser
+            ORDER BY totalScore DESC'
+        );
+
+        $joueurs = $query->getResult();
+
+        return $this->render('rank/rank.html.twig', [
+            'joueurs' => $joueurs,
+        ]);
+    }
+}
+*/
+
+
+namespace App\Controller;
+
+use App\Repository\UserRepository; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RankController extends AbstractController
+{
+    #[Route('/rank', name: 'ferrovipath_rank', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        $joueurs = $userRepository->findRanking();
+
+        return $this->render('rank/rank.html.twig', [
+            'joueurs' => $joueurs,
+        ]);
+    }
+}
+
+?>
