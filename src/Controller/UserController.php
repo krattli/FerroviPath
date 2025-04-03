6ff9cfaa52a3f41c4c8dfa566ad4915dc3a94eaa
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-04-03 14:03:53 +0200

    !!!! Important : Externalisation de tout ce qu'il y a dans UserController dans UserServices, j'ai pas encore tout testé si tout fonctionnais !!!

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,106 +14,52 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-final class UserController extends AbstractController{
-
-    public function __construct(private UserServices $user_services, private UserRepository $userRepository)
-    {
-        
-    }
+    /*
+     * J'ai externalisé la logique de toutes les routes dans la Classe UserServices
+     * Mais comme je connais pas tout le détail des fonctionnalités User je peux pas dire si tout fonctionne comme prévu
+     * J'ai testé un peu mais je garantis rien
+     * Signé : Raphael
+     */
+final class UserController extends AbstractController
+{
+    public function __construct(private UserServices $userServices) {}
 
     #[Route('/user/{id}/profil', name: 'ferrovipath_user_profil', methods: ['GET'])]
     public function profil(User $user): Response
-    {        
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
-        if($this->user_services->isTheConnectedUser($user) || $this->user_services->isSuperAdmin()){
-            return $this->render('user/profil.html.twig',  ['profil' => $user]);
-        }
-        else{   
-            $this->addFlash("errorAccess", "Vous n'avez pas accès à cette page");
-            return $this->redirectToRoute('ferrovipath_homepage');
-        }
+    {
+        return $this->userServices->handleProfil($user);
     }
 
     #[Route('/user/{id}/modify', name: 'ferrovipath_user_modify', methods: ['GET', 'POST'])]
-    public function modify(Request $request, User $id): Response 
-    {     
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');   
-        $isSuperAdmin = $this->user_services->isSuperAdmin();
-        if(!($this->user_services->isTheConnectedUser($id) || $isSuperAdmin)){
-            $this->addFlash("errorAccess", "Vous n'avez pas accès à cette page");
-            return $this->redirectToRoute('ferrovipath_homepage');
-        }
-        $form = $this->createForm(UserType::class, $id, ['is_edit' => true, 'is_super_admin' => $isSuperAdmin]);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($this->user_services->isSuperAdmin()){
-                $this->user_services->modifyProfilWithoutConfirmation($id, $form->get('plainPassword')->getData());
-                $this->addFlash('success', 'Le profil de ' . $id->getPseudo() . ' a été modifié !');
-                return $this->redirectToRoute('ferrovipath_admin_dashboard');
-            }
-            else if ($this->user_services->isTheConnectedUser($id)) {
-                $answer = $this->user_services->modifyProfil($id,$form->get('oldPassword')->getData(),$form->get('plainPassword')->getData());
-                if($answer==false){
-                    return $this->render('user/modify.html.twig', [
-                        'modifyForm' => $form->createView(), 'profil' => $id , 'wrongPasswordMessage'=>'Ancien mot de passe incorrecte, veuillez réessayer'
-                    ]);
-                } 
-            }
-            $this->addFlash('success','Modification du profil réussi !');
-            return $this->redirectToRoute('ferrovipath_user_profil', ['id' => $id->getIdUser()]);
-        }
-        return $this->render('user/modify.html.twig', [
-            'modifyForm' => $form->createView(), 'profil' => $id
-        ]);
-    }
-    
-    #[Route('/user/{id}/delete', name: 'ferrovipath_user_delete', methods: ['GET'])]
-    public function delete(User $user): Response //delete
+    public function modify(Request $request, User $user): Response
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
-        /*// Hard delete
-        $entityManager->remove($user);*/
-        if(!($this->user_services->isTheConnectedUser($user) || $this->user_services->isSuperAdmin())){
-            $this->addFlash("Vous ne pouvez pas supprimer le compte d'une autre personne","errorAccess");
-            return $this->redirectToRoute('ferrovipath_homepage');
-        }
-        $this->user_services->deleteUser($user);
-        $this->addFlash("success","Suppression du compte réussi !");
-        return $this->redirectToRoute('ferrovipath_homepage');
+        return $this->userServices->handleModify($request, $user);
     }
 
-    #[Route('/user/register', name: 'ferrovipath_register')] // Create
+    #[Route('/user/{id}/delete', name: 'ferrovipath_user_delete', methods: ['GET'])]
+    public function delete(User $user): Response
+    {
+        return $this->userServices->handleDelete($user);
+    }
+
+    #[Route('/user/register', name: 'ferrovipath_register')]
     public function register(Request $request): Response
     {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-        
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->user_services->registerUser($user, $form->get('plainPassword')->getData());
-            $this->addFlash('success', 'Vous êtes inscrit avec succès !');
-            return $this->redirectToRoute('ferrovipath_login');
-        }
-        return $this->render('user/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        return $this->userServices->handleRegister($request);
     }
-    
-    #[Route(path: '/user/login', name: 'ferrovipath_login')] 
+
+    #[Route(path: '/user/login', name: 'ferrovipath_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('user/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('user/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
     }
 
     #[Route(path: '/user/logout', name: 'ferrovipath_logout')]
-    public function logout(): void
-    {
-    }
-    
+    public function logout(): void {}
+
     #[Route('/user/logout/success', name: 'ferrovipath_logout_success')]
     public function logoutSuccess(): Response
     {
@@ -122,21 +68,8 @@ final class UserController extends AbstractController{
     }
 
     #[Route('/admin/user/{id}/roles', name: 'admin_user_roles')]
-    public function editRoles(User $user, Request $request, EntityManagerInterface $entityManager)
+    public function editRoles(User $user, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
-        $form = $this->createForm(UserRoleType::class, $user);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash('success', 'Rôles mis à jour avec succès.');
-            return $this->redirectToRoute('ferrovipath_admin_dashboard');
-        }
-        return $this->render('user/role.html.twig', [
-            'rolesForm' => $form->createView(),
-            'user' => $user
-        ]);
+        return $this->userServices->handleEditRoles($request, $user, $entityManager);
     }
-
 }
--- src/Service/UserServices.php
+++ src/Service/UserServices.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\User;
+use App\Form\UserRoleType;
+use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Twig\Environment;
 
-class UserServices{
-
-    public function __construct(private Security $security, private EntityManagerInterface $entityManager, private UserPasswordHasherInterface $userPasswordHasher){
-    }
+class UserServices
+{
+    public function __construct(
+        private Security $security,
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $userPasswordHasher,
+        private Environment $twig,
+        private FormFactoryInterface $formFactory
+    ) {}
 
     public function isSuperAdmin(): bool
     {
@@ -26,48 +38,155 @@ class UserServices{
     public function isTheConnectedUser(User $user): bool
     {
         $currentUser = $this->security->getUser();
-    
+
         if (!$currentUser instanceof User) {
-            return false; // Nécessaire pour trantyper UserInterface en User
-        }
-    
-        return $user->getIdUser() === $currentUser->getIdUser();
-    }
-    
-    public function modifyProfil($id,$oldPassword,$newPassword):bool
-    {
-        if(!$this->userPasswordHasher->isPasswordValid($id, $oldPassword)){
             return false;
         }
-        if(!empty($newPassword)){
-            $id->setPassword($this->userPasswordHasher->hashPassword($id, $newPassword));
+
+        return $user->getIdUser() === $currentUser->getIdUser();
+    }
+
+    public function modifyProfil(User $user, string $oldPassword, string $newPassword): bool
+    {
+        if (!$this->userPasswordHasher->isPasswordValid($user, $oldPassword)) {
+            return false;
+        }
+        if (!empty($newPassword)) {
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, $newPassword));
         }
         $this->entityManager->flush();
         return true;
     }
 
-    public function modifyProfilWithoutConfirmation (User $id, string $plainPassword):void
+    public function modifyProfilWithoutConfirmation(User $user, string $plainPassword): void
     {
-        $id->setPassword($this->userPasswordHasher->hashPassword($id, $plainPassword));
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, $plainPassword));
         $this->entityManager->flush();
     }
 
-    public function registerUser(User $user, string $plainPassword):bool
+    public function registerUser(User $user, string $plainPassword): bool
     {
-        // encode the plain password
         $user->setPassword($this->userPasswordHasher->hashPassword($user, $plainPassword));
-
         $this->entityManager->persist($user);
         $this->entityManager->flush();
-       return true;
+        return true;
     }
 
-    public function deleteUser(User $user):void
+    public function deleteUser(User $user): void
     {
         $user->setDeletedAt(new \DateTimeImmutable());
         $this->entityManager->flush();
     }
 
+    public function handleProfil(User $user): Response
+    {
+        if ($this->isTheConnectedUser($user) || $this->isSuperAdmin()) {
+            return new Response($this->twig->render('user/profil.html.twig', ['profil' => $user]));
+        }
+        throw new AccessDeniedException('Vous n\'avez pas accès à cette page');
+    }
+
+    public function handleModify(Request $request, User $user): Response
+    {
+        if (!$this->isTheConnectedUser($user) && !$this->isSuperAdmin()) {
+            throw new AccessDeniedException('Vous n\'avez pas accès à cette page');
+        }
+
+        $isSuperAdmin = $this->isSuperAdmin();
+        $form = $this->createForm(UserType::class, $user, ['is_edit' => true, 'is_super_admin' => $isSuperAdmin]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($isSuperAdmin) {
+                $this->modifyProfilWithoutConfirmation($user, $form->get('plainPassword')->getData());
+                return new Response($this->twig->render('user/modify.html.twig', [
+                    'modifyForm' => $form->createView(),
+                    'profil' => $user,
+                    'successMessage' => 'Le profil de ' . $user->getPseudo() . ' a été modifié !'
+                ]));
+            } elseif ($this->isTheConnectedUser($user)) {
+                $answer = $this->modifyProfil($user, $form->get('oldPassword')->getData(), $form->get('plainPassword')->getData());
+                if (!$answer) {
+                    return new Response($this->twig->render('user/modify.html.twig', [
+                        'modifyForm' => $form->createView(),
+                        'profil' => $user,
+                        'wrongPasswordMessage' => 'Ancien mot de passe incorrect, veuillez réessayer'
+                    ]));
+                }
+            }
+            return new Response($this->twig->render('user/modify.html.twig', [
+                'modifyForm' => $form->createView(),
+                'profil' => $user,
+                'successMessage' => 'Modification du profil réussi !'
+            ]));
+        }
+
+        return new Response($this->twig->render('user/modify.html.twig', [
+            'modifyForm' => $form->createView(),
+            'profil' => $user
+        ]));
+    }
+
+    public function handleDelete(User $user): Response
+    {
+        if (!$this->isTheConnectedUser($user) && !$this->isSuperAdmin()) {
+            throw new AccessDeniedException('Vous ne pouvez pas supprimer le compte d\'une autre personne');
+        }
+
+        $this->deleteUser($user);
+        return new Response($this->twig->render('user/delete.html.twig', [
+            'successMessage' => 'Suppression du compte réussi !'
+        ]));
+    }
+
+    public function handleRegister(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->registerUser($user, $form->get('plainPassword')->getData());
+            return new Response($this->twig->render('user/register.html.twig', [
+                'registrationForm' => $form->createView(),
+                'successMessage' => 'Vous êtes inscrit avec succès !'
+            ]));
+        }
+
+        return new Response($this->twig->render('user/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]));
+    }
+
+    public function handleEditRoles(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->isSuperAdmin()) {
+            throw new AccessDeniedException('Vous n\'avez pas accès à cette page');
+        }
+
+        $form = $this->createForm(UserRoleType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return new Response($this->twig->render('user/role.html.twig', [
+                'rolesForm' => $form->createView(),
+                'user' => $user,
+                'successMessage' => 'Rôles mis à jour avec succès.'
+            ]));
+        }
+
+        return new Response($this->twig->render('user/role.html.twig', [
+            'rolesForm' => $form->createView(),
+            'user' => $user
+        ]));
+    }
+
+    private function createForm(string $formType, $data, array $options = []): \Symfony\Component\Form\FormInterface
+    {
+        return $this->formFactory->create($formType, $data, $options);
+    }
 }
 
 ?>
