40429ddba07f9ffaf653f003ac1839a7647d5c5d
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-04-03 09:25:10 +0200

    première implémentation

---
+++ src/Controller/HistoryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HistoryController extends AbstractController
+{
+    #[Route('/history', name: 'ferrovipath_history', methods: ['GET'])]
+    public function history(EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            $this->addFlash('danger', 'Vous devez être connecté pour voir votre historique.');
+            return $this->redirectToRoute('ferrovipath_login');
+        }
+
+        $games = $entityManager->getRepository(Game::class)->findBy(
+            ['user' => $user],
+            ['createdAt' => 'DESC']
+        );
+
+        return $this->render('history/index.html.twig', [
+            'games' => $games,
+            'controller_name' => 'HistoryController',
+            'user' => $user,
+        ]);
+    }
+
+}
--- src/Controller/RankController.php
+++ src/Controller/RankController.php
@@ -2,29 +2,42 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository; 
+use App\Entity\Line;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RankController extends AbstractController
 {
     #[Route('/rank', name: 'ferrovipath_rank', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function rankByLine(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $query = $entityManager->createQuery(
-            'SELECT u.idUser, u.pseudo, SUM(g.scorePoints) as totalScore
+        // Récupère toutes les lignes présentes dans la BDD
+        $lines = $entityManager->getRepository(Line::class)->findAll();
+        $selectedLineId = $request->query->get('line');
+
+        $joueurs = [];
+
+        if ($selectedLineId) {
+            $query = $entityManager->createQuery(
+                'SELECT u.idUser, u.pseudo, SUM(g.scorePoints) as totalScore
             FROM App\Entity\Game g
             JOIN g.user u
+            WHERE g.line = :lineId AND u IS NOT NULL
             GROUP BY u.idUser
             ORDER BY totalScore DESC'
-        );
+            )->setParameter('lineId', $selectedLineId);
 
-        $joueurs = $query->getResult();
+            $joueurs = $query->getResult();
+        }
 
-        return $this->render('rank/rank.html.twig', [
+        return $this->render('rank/index.html.twig', [
             'joueurs' => $joueurs,
+            'lines' => $lines,
+            'selectedLineId' => $selectedLineId,
         ]);
     }
 }
