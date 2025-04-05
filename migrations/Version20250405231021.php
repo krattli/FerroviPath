2d8d30819b09a3dc71fb761b5e8337baf720a639
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2025-04-06 01:26:41 +0200

    feature vraiment innutile mais plus réaliste : le texte des logos de métro est noir ou blanc selon la luminance de la couleur de la ligne

--- migrations/Version20250405231021.php
+++ migrations/Version20250405231021.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250405151049 extends AbstractMigration
+final class Version20250405231021 extends AbstractMigration
 {
     public function getDescription(): string
     {
